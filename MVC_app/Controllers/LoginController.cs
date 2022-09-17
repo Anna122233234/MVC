4a46eddf1e9af0e17ac5c842d422b7ad316cbe93
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-09-17 21:48:18 +0300

    Added MVC project with all folders

---
+++ MVC_app/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_app.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+
+namespace MVC_app.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+        public IActionResult Check(UserModel user)
+        {
+            try
+            {
+                user.login = this.Request.Form["login"];
+                user.password = this.Request.Form["password"];
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+            foreach (var record in GetUser())
+            {
+                if (user.login == record.login && user.password == record.password)
+                {
+                    user.verif = true;
+                    user.admin = record.admin;
+                    return View("Checking", user);
+                }
+            }
+            return View("Checking", user);
+        }
+        public List<UserModel> GetUser()
+        {
+            List<UserModel> users = new List<UserModel>
+            {
+                new UserModel { login = "Anna", password = "1337", admin = false },
+                new UserModel { login = "Ann" , password = "228", admin = true },
+                new UserModel { login = "Ann1", password = "5678", admin = false },
+                new UserModel { login = "Ann2", password = "4578", admin = false },
+                new UserModel { login = "Ann3", password = "1245", admin = true },
+                new UserModel { login = "Ann4", password = "7859", admin = true }
+            };
+            return users;
+        }
+
+    }
+}
---
+++ MVC_app/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_app.Models
+{
+    public class UserModel
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+        public bool admin { get; set; }
+        public bool verif { get; set; }
+    }
+}
